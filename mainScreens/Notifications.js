184fd025ad9ab612dd5680af8d64edf35672d109
Austin Atmaja <austin.atmaja@gmail.com>
2019-03-27 13:59:48 -0400

    started developing notifications screen

--- mainScreens/Notifications.js
+++ mainScreens/Notifications.js
@@ -1,23 +1,40 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Dimensions, TouchableOpacity} from 'react-native';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 import {ActionCreators} from '../actions';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import BleManager from 'react-native-ble-manager';
 
 class Notifications extends Component{
   constructor(props){
     super(props);
-    this.state = {}
+    this.state = {
+        todayExpanded: false,
+        weekExpanded: false,
+        monthExpanded: false
+    }
   }
 
   componentDidMount(){
   }
 
   render() {
+      console.log(this.state.todayExpanded)
     return (
       <View style={styles.container}>
-        <Text>Notifications</Text>
+        <TouchableOpacity onPress={() => this.setState({todayExpanded: !this.state.todayExpanded})} style={styles.timePeriodContainer}>
+            <Text style={styles.timePeriodText}>Today </Text>
+            <Icon style={styles.collapseIcon} size={24} name={this.state.todayExpanded ? "chevron-up" : "chevron-down"}></Icon>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => this.setState({weekExpanded: !this.state.weekExpanded})} style={styles.timePeriodContainer}>
+            <Text style={styles.timePeriodText}>This Week </Text>
+            <Icon style={styles.collapseIcon} size={24} name={this.state.weekExpanded ? "chevron-up" : "chevron-down"}></Icon>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => this.setState({monthExpanded: !this.state.monthExpanded})} style={styles.timePeriodContainer}>
+            <Text style={styles.timePeriodText}>This Month </Text>
+            <Icon style={styles.collapseIcon} size={24} name={this.state.monthExpanded ? "chevron-up" : "chevron-down"}></Icon>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -26,8 +43,24 @@ class Notifications extends Component{
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'center'
+  },
+  timePeriodContainer: {
+      width: Dimensions.get('window').width,
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      height: 60,
+      borderBottomColor: "black", 
+      borderBottomWidth: (StyleSheet.hairlineWidth)*2
+  },
+  timePeriodText: {
+      fontWeight: 'bold',
+      fontSize: 18,
+      marginLeft: 16
+  },
+  collapseIcon: {
+      marginRight: 16
   }
 });
 
