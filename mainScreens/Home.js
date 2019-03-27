bff38d13962a6f3d56982f74c3e9e61eea1f81d5
Austin Atmaja <austin.atmaja@gmail.com>
2019-03-27 16:22:01 -0400

    add home screen functionality

--- mainScreens/Home.js
+++ mainScreens/Home.js
@@ -4,36 +4,73 @@ import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 import {ActionCreators} from '../actions';
 import BleManager from 'react-native-ble-manager';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 
 class Home extends Component{
   constructor(props){
     super(props);
-    this.state = {}
+    this.state = {
+        patients: [
+            {name: "William Neil", id: 1, location: {latitude: 43.4729, longitude: -80.5400}, isStressed: true},
+            {name: "Leslie Johnsonberg", id: 2, location: {latitude: 43.4728, longitude: -80.5400}, isStressed: true},
+            {name: "Brock Thorn", id: 3, location: {latitude: 43.4729, longitude: -80.5401}, isStressed: false},
+            {name: "Jasmine Kepernick", id: 4, location: {latitude: 43.4727, longitude: -80.54005}, isStressed: false},
+            {name: "Casper Seretoni", id: 5, location: {latitude: 43.4729, longitude: -80.5399}, isStressed: false},
+        ]
+    }
   }
 
   componentDidMount(){
+  }
+
+  renderPatientStatus(){
+    return this.state.patients.map((patient) => {
+        return (
+            <View style={{flexDirection: 'row', marginHorizontal: 8, borderWidth: StyleSheet.hairlineWidth, borderColor: "#d3d3d3"}}>
+                <View style={{flex: 0, width: 10, backgroundColor: 'blue', marginRight: 15}}></View>
+                <View style={{paddingVertical: 8, flex: 1, flexDirection: 'row'}}>
+                    <View style={{flex: 1}}>
+                        <Text style={{fontSize: 18}}>{patient.name}</Text>
+                    </View>
+                    <View style={{flex: 0, marginHorizontal: 15}}>
+                        <View style={[styles.statusDot, {backgroundColor: patient.isStressed ? 'red' : 'green'}]}></View>
+                    </View>
+                </View>
+            </View>
+        )
+    })
   }
 
   render() {
     const screenWidth = Dimensions.get('window').width;
     return (
         <View>
-            <View style={[styles.container, {width: screenWidth}]}>
+            <Text style={styles.subtitle}>Patient Location</Text>
+            <View style={[styles.containerTop, {width: screenWidth}]}>
                 <MapView
                     provider={PROVIDER_GOOGLE} // remove if not using Google Maps
                     style={{...StyleSheet.absoluteFillObject}}
                     region={{
-                        latitude: 37.78825,
-                        longitude: -122.4324,
-                        latitudeDelta: 0.015,
-                        longitudeDelta: 0.0121,
+                        latitude: 43.472923,
+                        longitude: -80.540087,
+                        latitudeDelta: 0.001,
+                        longitudeDelta: 0.001,
                     }}
                 >
+                    {this.state.patients.map((patient) => {
+                        return(
+                            <Marker
+                                coordinate={patient.location}
+                                title={patient.name}
+                                pinColor={patient.isStressed ? 'red' : 'green'}
+                            />
+                        )
+                    })}
                 </MapView>
             </View>
-            <View style={styles.container}>
-                    <Text>Home</Text>
+            <Text style={styles.subtitle}>Patient Status</Text>
+            <View style={styles.containerBottom}>
+                {this.renderPatientStatus()}
             </View>
         </View>
     );
@@ -41,11 +78,25 @@ class Home extends Component{
 }
 
 const styles = StyleSheet.create({
-  container: {
+  containerTop: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
+  containerBottom: {
+    flex: 1,
+  },
+  subtitle: {
+      marginLeft: 16,
+      fontWeight: 'bold',
+      fontSize: 18,
+      marginVertical: 8
+  },
+  statusDot: {
+    height: 20,
+    width: 20,
+    borderRadius: 20,
+  }
 });
 
 function mapStateToProps(state) {
